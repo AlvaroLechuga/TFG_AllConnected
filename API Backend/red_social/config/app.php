a67df9329d0da65127e5b5a16625cf025a3a0dd6
Álvaro Lechuga <45164539+AlvaroLechuga@users.noreply.github.com>
2019-08-14 15:58:50 +0200

    a

--- API Backend/red_social/config/app.php
+++ API Backend/red_social/config/app.php
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', base64_decode('YlXVebBH0OSzI+TLM3P9ELhJumZtvh/OLBYjGWZwpE4=')),
 
     'cipher' => 'AES-256-CBC',
 
